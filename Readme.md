08668d6ff58606e008f0c4c0c8d330dc302cf30d
admin <dangtinhtong@gmail.com>
2017-11-12 00:35:49 +0700

    First commit program

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This is a small program that displays message "Programming is fun!" and "Programming in Objective C is even more fun!" into 2 lines
--- prog2.2/main.m
+++ prog2.2/main.m
@@ -6,12 +6,17 @@
 //  Copyright © 2017 admin. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Foundation/Foundation.h>  // This is a system file
+                                   // #import says to import or include the information from that file into the program, exactly as if the contents of the file were typed into program at that point
+                                   // imported the file Foundation.h because it has information about other classes and functions that are used later in the program
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])  // is a special name that indicates precisely where the program is to begin excution.
+  {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+       // NSLog(@"Hello, World!");
+        NSLog(@"Programming is fun!");
+        NSLog(@"Programming in Objective C is even more fun!");
     }
     return 0;
 }
